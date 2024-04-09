d0ca474bd97da3dcf6b3739adc1f708bdf1b95cb
Amanda Domeru Egega <amandaegega1@gmail.com>
2024-04-09 09:56:34 +0100

    Add files via upload

---
+++ data merging.sql
@@ -0,0 +1,32 @@
+#Merge all tables from Quarter 1 2020 to December 2020
+
+  SELECT *
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_Q1`
+  UNION ALL
+  SELECT * 
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_04`
+  UNION ALL
+  SELECT *
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_05`
+  UNION ALL
+  SELECT * 
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_06`
+  UNION ALL
+  SELECT * 
+  FROM `case-study-419113.tripdata2020_case_study.trip-data_2020_07`
+  UNION ALL
+  SELECT * 
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_08`
+  UNION ALL
+  SELECT *
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_09`
+  UNION ALL
+  SELECT *
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_10`
+  UNION ALL
+  SELECT * 
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_11`
+  UNION ALL
+  SELECT * 
+  FROM `case-study-419113.tripdata2020_case_study.trip_data_2020_12_v3`
+
