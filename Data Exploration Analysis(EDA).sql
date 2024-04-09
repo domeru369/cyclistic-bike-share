cf478057c8fa7d950f85177a511a3951d939f0af
Amanda Domeru Egega <amandaegega1@gmail.com>
2024-04-09 12:15:17 +0100

    Update Data Exploration Analysis(EDA).sql

--- Data Exploration Analysis(EDA).sql
+++ Data Exploration Analysis(EDA).sql
@@ -1,80 +1,81 @@
-#data exploration
+--data exploration
 
-#checking the number of rows in the data
+--checking the number of rows in the data
 SELECT 
     COUNT (*)
 FROM `case-study-419113.tripdata2020_case_study.merged_data`
-#there are 3,541,683 rows
+--there are 3,541,683 rows
 
+    
 
-#to check all columns data types
+--to check all columns data types
 SELECT 
     column_name,data_type
 FROM `tripdata2020_case_study`.INFORMATION_SCHEMA.COLUMNS
 WHERE table_name = 'merged_data'
 
+    
 
-#since ride id is a unique column we need to check if the lengths are consistent
+--since ride id is a unique column we need to check if the lengths are consistent
 SELECT 
     LENGTH(ride_id) AS length_ride_id
 FROM `case-study-419113.tripdata2020_case_study.merged_data`  
-#most of ride ids are 16  
+--most of ride ids are 16  
 
-#to check the ones that are not 16 
+--to check the ones that are not 16 
 SELECT 
    COUNT(ride_id)
 FROM
     `case-study-419113.tripdata2020_case_study.merged_data`
 WHERE 
     LENGTH(ride_id) != 16
-#there are 2401 ride ids that are not 16  
+--there are 2401 ride ids that are not 16  
 
 
 
-#check for duplicate rows
-#to get distinct rides 
+--check for duplicate rows
+--to get distinct rides 
 SELECT
     COUNT(DISTINCT ride_id) AS num_of_rides 
 FROM 
     `case-study-419113.tripdata2020_case_study.merged_data`
-#there are 3,541,470 distinct rides
+--there are 3,541,470 distinct rides
 
-#subtraction
+--subtraction
 SELECT 
     COUNT(ride_id) - COUNT(DISTINCT ride_id) AS duplicate_rows
 FROM 
     `case-study-419113.tripdata2020_case_study.merged_data`    
-#there are 213 duplicate rows 
+--there are 213 duplicate rows 
 
-#to get distinct rideable types
+--to get distinct rideable types
 SELECT rideable_type,
     COUNT(DISTINCT rideable_type) AS num_of_rideabletypes
 FROM `case-study-419113.tripdata2020_case_study.merged_data`
 GROUP BY rideable_type  
-#there are 3 unique rideable types 
+--there are 3 unique rideable types 
 
-#to get distinct start stations
+--to get distinct start stations
 SELECT
     COUNT(DISTINCT start_station_id) AS num_of_start_stations
 FROM `case-study-419113.tripdata2020_case_study.merged_data` 
-#there are 931 start stations
+--there are 931 start stations
 
-#to get distinct end stations
+--to get distinct end stations
 SELECT
     COUNT(DISTINCT end_station_id) AS num_of_end_stations
 FROM `case-study-419113.tripdata2020_case_study.merged_data` 
-#there are 931 end stations
+--there are 931 end stations
 
-#to get distinct riders
+--to get distinct riders
 SELECT member_casual,
     COUNT(DISTINCT member_casual) AS num_of_rider_types
 FROM `case-study-419113.tripdata2020_case_study.merged_data`
 GROUP BY member_casual 
-#there are 2 types of riders; casual and member
+--there are 2 types of riders; casual and member
 
 
-#checking for number of null values in all columns
-
+--checking for number of null values in all columns
 SELECT COUNT(*) - COUNT(ride_id) AS ride_id,
  COUNT(*) - COUNT(rideable_type) AS rideable_type,
  COUNT(*) - COUNT(started_at) AS started_at,
@@ -89,26 +90,30 @@ SELECT COUNT(*) - COUNT(ride_id) AS ride_id,
  COUNT(*) - COUNT(end_lng) AS end_lng,
  COUNT(*) - COUNT(member_casual) AS member_casual
 FROM `case-study-419113.tripdata2020_case_study.merged_data`
+--16492 start stations are missing
+--179615 end stations are misssing  
+--both end lat and end log values are missing  4255 values    
+    
 
 
-#to get rides longer than a day
+--to get rides longer than a day
 SELECT COUNT(*) AS longer_than_a_day
 FROM `case-study-419113.tripdata2020_case_study.merged_data`
 WHERE (
   EXTRACT(HOUR FROM (ended_at - started_at)) * 60 +
   EXTRACT(MINUTE FROM (ended_at - started_at)) +
   EXTRACT(SECOND FROM (ended_at - started_at)) / 60) >= 1440;
-#2861 rides are longer than a day 
+--2861 rides are longer than a day 
 
 
-#to get rides less than a minute
+--to get rides less than a minute
 SELECT COUNT(*) AS less_than_a_minute
 FROM `case-study-419113.tripdata2020_case_study.merged_data`
 WHERE (
   EXTRACT(HOUR FROM (ended_at - started_at)) * 60 +
   EXTRACT(MINUTE FROM (ended_at - started_at)) +
   EXTRACT(SECOND FROM (ended_at - started_at)) / 60) <= 1;
-#there are 76372 rides less than a minute 
+--there are 76372 rides less than a minute 
 
 
 
